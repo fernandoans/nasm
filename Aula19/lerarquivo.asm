1d64ea4647f1d45df3d7c107d0f4459adce12a56
Fernando Anselmo <fernando.anselmo74@gmail.com>
2021-04-25 13:12:42 -0300

    [Add] Aula 19

---
+++ Aula19/lerarquivo.asm
@@ -0,0 +1,44 @@
+;--------------------------------------
+; Ler o Arquivo
+;--------------------------------------
+%include 'bibliotecaE.inc'
+
+SECTION .data
+    arq     db "Musica.txt", NULL
+    tam     equ 1024    ; Tam do Arquivo
+
+SECTION .bss
+    fd      resb 4      ; File Descriptor
+    buffer  resb 1024   ; Descarregar Memória
+
+SECTION .text
+
+global _start:
+
+_start:
+    mov     eax, OPEN_FILE
+    mov     ebx, arq
+    mov     ecx, STDIN
+    int     SYS_CALL
+
+    mov     [fd], eax  ; Armazenando o File Descriptor
+
+    mov     eax, READ_FILE
+    mov     ebx, [fd]
+    mov     ecx, buffer
+    mov     edx, tam
+    int     SYS_CALL
+
+    mov     eax, SYS_WRITE
+    mov     ebx, STDOUT
+    mov     ecx, buffer
+    mov     edx, tam
+    int     SYS_CALL
+
+    mov     eax, CLOSE_FILE
+    mov     ebx, [fd]
+    int     SYS_CALL
+
+    mov     eax, SYS_EXIT
+    mov     ebx, EXIT_SUCESS
+    int     SYS_CALL
