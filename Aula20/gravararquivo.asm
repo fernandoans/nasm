b20facb6eef7da5ec1d3d932d6f54524be84f520
Fernando Anselmo <fernando.anselmo74@gmail.com>
2021-05-01 21:30:24 -0300

    [Add] Aula 20 e última? (vcs vão decidir)

---
+++ Aula20/gravararquivo.asm
@@ -0,0 +1,40 @@
+;--------------------------------------
+; Gravar o Arquivo
+;--------------------------------------
+%include 'bibliotecaE.inc'
+
+SECTION .data
+    msg     db "Hello World! Voltamos ao Início...", LF
+    tamMsg  equ $ - msg
+    arq     db 'Hello', NULL
+    tamArq  equ $ - arq
+    fd      dq 0 ; File Descriptor
+
+SECTION .text
+
+global _start:
+
+_start:
+  mov eax, CREATE_FILE
+  mov ebx, arq
+  mov ecx, 0o664  ; 1 x  2 w  3 wx 4 r 5 rx 6 rw 7 rwx
+  mov edx, tamArq
+  int SYS_CALL
+
+escrever:
+  mov [fd], eax  
+  mov eax, WRITE_FILE
+  mov ebx, [fd]
+  mov ecx, msg  ; 1 x  2 w  3 wx 4 r 5 rx 6 rw 7 rwx
+  mov edx, tamMsg
+  int SYS_CALL
+
+fechar:
+  mov eax, CLOSE_FILE
+  mov ebx, [fd]
+  int SYS_CALL
+
+termino:
+  mov eax, SYS_EXIT
+  mov ebx, EXIT_SUCESS
+  int SYS_CALL
